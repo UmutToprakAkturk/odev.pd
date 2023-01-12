ea5627fa1e382cd8aad8cbde38fa331ef7082389
Umut Toprak AKTÜRK <toprak.akturk765@gmail.com>
2023-01-12 11:56:42 +0300

    düzeltilmişmatrisçdev1
    kodlarda düzeltmeler yapıldı ilerleme kaydedildi.

--- matris.ödevibir/main.c
+++ matris.ödevibir/main.c
@@ -4,29 +4,31 @@
 
 int main()
 {
-    int M,N,i,j,k,l,f,u,o,p,t,q,w,a,s;
-    s: printf("Matriste satır sayısını girniz\n");
+    int M=0,N=0,i=0,j=0,k=0,l=0,f=0,u=0,o=0,p=0,t=0,b=0,q=0,w=0,a=0,s=0,m=0,n=0;
+    y: printf("Matriste satÃ½r sayÃ½sÃ½nÃ½ girniz\n");
     scanf("%d",&M);
-    printf("Matriste sutün sayısını girniz\n");
+    printf("Matriste sutÃ¼n sayÃ½sÃ½nÃ½ girniz\n");
     scanf("%d",&N);
     if (M<3)
-    {printf("lütfen 3ten büyük deger giriniz\n");
-    goto s;
+    {printf("lÃ¼tfen 3ten bÃ¼yÃ¼k deger giriniz\n");
+    goto y;
     }
     else if (N<3)
-    {printf("lütfen 3ten büyük deger giriniz\n");
-    goto s;
+    {printf("lÃ¼tfen 3ten bÃ¼yÃ¼k deger giriniz\n");
+    goto y;
     }
-    int H[M-1][N-1],A[M][N],G[3][3];
+    m=M-2;
+    n=N-2;
+    int H[m][n],A[M][N],G[3][3];
     for (k=0;k<3;k++)
-            {printf("filtre elemanlarını girin");
+            {printf("filtre elemanlarÃ½nÃ½ girin");
              scanf("%d",&o);
              G[k][u]=o;
                 for (u=1;u<3;u++)
                 {
-                 printf("filtre elemanlarını girin");
+                 printf("filtre elemanlarÃ½nÃ½ girin");
                  scanf("%d",&p);
-                 G[k][u]=p;
+                 G[k][u]=p; //g matrisi filtredir
                     }
                     u=0;
                     }
@@ -42,35 +44,42 @@ int main()
                 printf("A[%d][%d]=%d \n",i,j,A[i][j]);
             }
     }
-   for (f=0;f<M-2;f++)
+   for (a=0;a<m;a++)//burda filtre matrisinin degerlerini sabit tutarken carpÄ±lacak olan matrisin diger elemenalarÄ±na geÃ§meye calÄ±stÄ±m
     {
-        for (l=0;l<N-2;l++)
+        for (s=0;s<n;s++)
         {       for (f=0;f<3;f++)
             {
-                t=+G[f][l]*A[f][l];
+                t=+G[f][l]*A[a][s];
+                b=t;
                         for (l=1;l<3;l++)
                         {
-                        t=+G[f][l]*A[f][l];
+                        s++;
+                        t=+G[f][l]*A[a][s];
+                        b=t;
                         }
                         l=0;
+                        s=s-2;
+                        a++;
             }
+            a=a-3;
     H[q][w]=t;
     w++;
     t=0;
     }
     w=0;
-    H[q][w]=t;
+    H[q][w]=b;
     q++;
-    }
+    b=0;
+    }//H gÃ¶rÃ¼ntÃ¼ matrisine deger verdim ancak sadece son filtre degerinin cÄ±ktÄ±sÄ±nÄ± alÄ±yorum.
 
-    for (a=0;a<M-2;a++)
-    {  for(s=0;s<N-2;s++)
+    for (q=0;q<M-2;q++)
+    {  for(w=0;w<N-2;w++)
             {
-            printf("H[%d][%d]=%d",a,s,H[a][s]);
+            printf("H[%d][%d]=%d",q,w,H[q][w]);
             }
     }
 
 
     return 0;
 }
-//çalışmıyor hatayı bulamadım :/
+//Ã§alÃ½Ã¾mÃ½yor hatayÃ½ bulamadÃ½m :/
